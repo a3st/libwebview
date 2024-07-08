e916c13bbe26852b51169b632f45b1a594aa67e8
Dmitriy <mludima23@gmail.com>
2024-07-08 21:18:00 +0800

    Linux Support

--- native/src/platform/webkit.cpp
+++ native/src/platform/webkit.cpp
@@ -15,6 +15,9 @@ namespace libwebview
         gtk_window_set_title(window, std::string(title).c_str());
         gtk_window_set_default_size(window, width, height);
         gtk_window_set_resizable(window, resizeable);
+
+        webview = reinterpret_cast<WebKitWebView*>(webkit_web_view_new());
+        gtk_window_set_child(window, GTK_WIDGET(webview));
     }
 
     auto WebKit::setWindowMaxSize(uint32_t const width, uint32_t const height) -> void
@@ -31,30 +34,33 @@ namespace libwebview
 
     auto WebKit::run(std::string_view const urlPath) -> void
     {
-        /*GtkApplication* application = gtk_application_new(std::string(appName).c_str(), G_APPLICATION_DEFAULT_FLAGS);
+        if (urlPath.starts_with("http://") || urlPath.starts_with("https://"))
+        {
+            webkit_web_view_load_uri(webview, std::string(urlPath).c_str());
+        }
+        else
+        {
+            std::filesystem::path const currentPath = std::filesystem::current_path();
+            webkit_web_view_load_uri(webview, ("file:///" + (currentPath / urlPath).generic_string()).c_str());
+        }
 
-        g_signal_connect(application, "activate", G_CALLBACK(*[](GtkApplication* application, gpointer userData) {
-                             auto platformInstance = reinterpret_cast<WebKit*>(userData);
+        bool running = true;
 
-                             if (!platformInstance)
-                             {
-                                 return;
-                             }
-
-                             gtk_application_add_window(application, platformInstance->window);
+        g_signal_connect(window, "destroy", G_CALLBACK(*[](GtkWidget* widget, gpointer userData) {
+                             auto running = reinterpret_cast<bool*>(userData);
+                             (*running) = false;
                          }),
-                         this);
-
-        g_application_run(G_APPLICATION(application), 0, nullptr);*/
+                         &running);
 
         gtk_window_present(window);
 
-        bool running = true;
         while (running)
         {
-            
-
-            g_main_context_iteration(nullptr, true);
+            g_main_context_iteration(nullptr, false);
+            if (idleCallback)
+            {
+                idleCallback();
+            }
         }
     }
 
